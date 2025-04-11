417580599b4110fbb5cbabc30dce175a63b308aa
Dawn Schumacher <dawn@berkeley.edu>
2025-04-11 15:34:47 -0700

    Correctly label content

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const textbook = defineCollection({
-    loader: glob({ pattern: "**/*.mdx", base: "./content/"}),
+    loader: glob({ pattern: "**/*.mdx", base: "./content-legacy/"}),
     schema: z.object({
         title: z.string(),
         section: z.number()
