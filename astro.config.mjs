c706496d4a4966229c9f17605c84b48592bbc362
Dawn Schumacher <dawn@berkeley.edu>
2025-04-10 18:20:32 -0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+      shikiConfig: {
+          theme: 'gruvbox-dark-medium',
+      },
+  },
+
+  integrations: [mdx(), svelte()],
+});
