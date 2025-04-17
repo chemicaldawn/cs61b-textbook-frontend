731dce3a196b2d3da58d38b7902226287fad3782
Dawn Schumacher <dawn@berkeley.edu>
2025-04-17 16:39:37 -0700

    Union bug

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,8 @@ const textbook = defineCollection({
     loader: glob({ pattern: "**/*.mdx", base: "./content-legacy/"}),
     schema: z.object({
         title: z.string(),
+        authors: z.string().array().default([]),
+        union_bug : z.boolean().default(false),
         section: z.number(),
         slug: z.string()
     })
