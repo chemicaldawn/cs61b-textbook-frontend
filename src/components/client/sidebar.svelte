cbf1cdf503d038ce42eebea4101c7350b734fc4b
Dawn Schumacher <dawn@berkeley.edu>
2025-04-18 21:09:56 -0700

    Massively improved mobile experience

--- src/components/client/sidebar.svelte
+++ src/components/client/sidebar.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import SidebarGroup from "./sidebar-group.svelte";
 
     let { groups } = $props();
+    let open = $state(false)
     let selected = $state("")
+
+    onMount(() => {
+        document.getElementById("hamburger")?.addEventListener("click", () => {
+            open = !open
+        })
+    })
 </script>
 
-{#each groups.values() as g}
-    <SidebarGroup group={g} bind:selected></SidebarGroup>
-{/each}
+<div id="sidebar" class={open ? "open" : ""}>
+    <div class="sidebar-title">Table of Contents</div>
+    {#each groups.values() as g}
+        <SidebarGroup group={g} bind:selected></SidebarGroup>
+    {/each}
+</div>
